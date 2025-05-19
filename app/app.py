78c931018b3d727558eadc44eb96d902e19ca2cb
mancrurod <mcruzrodriguez794@gmail.com>
2025-05-19 14:28:56 +0200

    hid header again

--- app/app.py
+++ app/app.py
@@ -90,7 +90,9 @@ def set_background(image_path: str) -> None:
                 transition: all 0.3s ease;
             }}
 
-
+            header[data-testid="stHeader"] {{
+                display: none;
+            }}
 
             span[data-testid="stFormSubmitHelper"] {{
                 display: none !important;
@@ -104,10 +106,6 @@ def set_background(image_path: str) -> None:
         """, unsafe_allow_html=True)
     except FileNotFoundError:
         st.warning("⚠️ Background image not found. Please check the path.")
-
-            # header[data-testid="stHeader"] {{
-            #     display: none;
-            # }}
 
 def inject_custom_styles() -> None:
     """
